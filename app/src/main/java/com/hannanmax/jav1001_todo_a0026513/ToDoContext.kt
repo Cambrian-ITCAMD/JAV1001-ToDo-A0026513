48fbbd48ca50aa44a17ed3a7aa0c584f34e2c1d4
Hannan Max <48342941+hannanmax@users.noreply.github.com>
2022-11-10 16:06:38 -0500

    Updated ToDoCOntext.kt
    Now has better getter setter in main kotlin documentation format.

--- app/src/main/java/com/hannanmax/jav1001_todo_a0026513/ToDoContext.kt
+++ app/src/main/java/com/hannanmax/jav1001_todo_a0026513/ToDoContext.kt
@@ -1,27 +1,14 @@
 package com.hannanmax.jav1001_todo_a0026513
 
-class ToDoContext {
-    var title: String? = ""
-    var isCompleted: Boolean? = false
-
-    constructor(title: String, isCompleted: Boolean) {
-        this.title = title
-        this.isCompleted = isCompleted
+class ToDoContext(title: String, isCompleted: Boolean) {
+    var title: String? = title
+    get() = field
+    set(value) {
+        field = value
     }
-
-    fun gettitle(): String? {
-        return title
-    }
-
-    fun settitle(title: String?) {
-        this.title = title
-    }
-
-    fun getisCompleted(): Boolean? {
-        return isCompleted
-    }
-
-    fun setIsCompleted(isCompleted: Boolean?) {
-        this.isCompleted = isCompleted
+    var isCompleted: Boolean? = isCompleted
+    get() = field
+    set(value) {
+        field = value
     }
 }
